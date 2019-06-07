bd0f21c96cdbc035c488fdcd56aaf24483e2b48e
Vlad Slyusar (laptop) <fmfrankenschtein@gmail.com>
2019-06-07 21:17:38 +0300

    Class work 07.06.2019 nothing int

---
+++ Class  Work/Arrays/maxNum.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printArray ( int A [], int size )
+{
+    int i;
+    for ( i = 0; i < size; i++ )
+    {
+        printf ( "Array [%i] = %i\n", i, A [i] );
+    }
+    printf ( "\n" );
+}
+
+void sortArrayMax ( int a[], int size )
+{
+    int index, j, i, temp;
+    for ( j = 0; j < 4; j++ )
+    {
+        index = j;
+        for ( i = j + 1; i < 5; i++ )
+        {
+            if ( a[index] < a [i] )
+                index = i;
+        }
+        temp = a [index];
+        a [index] = a [j];
+        a [j] = temp;
+    }
+}
+void sortArrayMin ( int a[], int size )
+{
+    int index, j, i, temp;
+    for ( j = 0; j < 4; j++ )
+    {
+        index = j;
+        for ( i = j + 1; i < 5; i++ )
+        {
+            if ( a[index] > a [i] )
+                index = i;
+        }
+        temp = a [index];
+        a [index] = a [j];
+        a [j] = temp;
+    }
+}
+
+
+int main()
+{
+    int a [5] = {5, 8, 3, 9, 7};
+    sortArrayMax ( a, 5 );
+    printArray ( a, 5 );
+    sortArrayMin ( a, 5 );
+    printArray ( a, 5 );
+    return 0;
+}
--- Class  Work/SumOfNum.c
+++ Class  Work/SumOfNum.c
@@ -2,19 +2,20 @@
 #include <conio.h>
 #include <math.h>
 
-void fact ( int num){
-    int i, factorial = 1;
-    for (i = 1; i <= num; i++){
-		factorial *= i;
+void sumofNum ( int num){
+    int i, sum = 0;
+    for (i = 1; 0 < num; i++){
+		sum += num %10;
+		num /= 10;
 	}
-	printf ("Fact = %i", factorial);
+	printf ("Fact = %i", sum);
 }
 int main ()
 {
 	int i, factorial = 1,number ;
 	printf ("Enter number: ");
 	scanf  ("%i", &number);
-	fact(number);
+    sumofNum(number);
 	getchar();
 	return 0;
 }
