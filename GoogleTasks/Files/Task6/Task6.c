e4266f7d0e544541dabf4ef33b3fab4b072f5e1a
Vlad Slyusar <fmfrankenschtein@gmail.com>
2019-07-19 17:26:09 +0300

    HW

---
+++ GoogleTasks/Files/Task6/Task6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    FILE *input = fopen("input.txt", "r");
+    FILE *validNumbers = fopen("validNumbers.txt", "w");
+    FILE *integers = fopen("integers.txt", "w");
+    float tempFloat, sumFloat = 0, lastValidNum;
+    int tempInt, sumInt = 0, sumIntParts = 0;
+
+    while(1){
+        if (fscanf(input, "%f", &tempFloat) == EOF) break;
+        tempInt = tempFloat;
+        if (tempFloat - tempInt == 0){
+            fprintf(integers, "%i\n", tempInt);
+            sumInt += tempInt;
+        }else {
+            fprintf(validNumbers, "%g\n", tempFloat);
+            sumFloat += tempFloat;
+            sumIntParts += tempInt;
+        }
+    }
+    fprintf(integers, "Sum = %i\n", sumInt);
+    fprintf(validNumbers, "Sum = %g\n", sumFloat);
+    freopen("validNumbers.txt", "a",validNumbers );
+    freopen("validNumbers.txt", "r",input );
+    rewind(validNumbers);
+    lastValidNum = tempFloat;
+    fprintf(validNumbers, "Integer parts: ");
+    while(1){
+        if (fscanf(input, "%f", &tempFloat) == EOF ) break;
+        tempInt = tempFloat;
+        fprintf(validNumbers, "%i ", tempInt);
+        if (tempFloat == lastValidNum) break;
+    }
+    fprintf(validNumbers, "\nSum of integer parts: %i", sumIntParts);
+    fclose(input);
+    fclose(validNumbers);
+    fclose(integers);
+    return 0;
+}
