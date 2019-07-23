876e5f4f38ea88b7ceb1cd4182faf973acaaddf6
Vlad Slyusar <fmfrankenschtein@gmail.com>
2019-07-23 17:26:00 +0300

    HW

---
+++ GoogleTasks/Files/binary/Task2/Task2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define N 15
+#define M 15
+
+int main()
+{
+    FILE* fp = fopen("input.dat", "rb");
+    FILE* fpOutput = fopen("output.dat", "wb");
+    int A[N] = {}, i = 0, j, B[M] = {}, temp;
+    int C[N];
+    while (1){
+        if ( i == 0){
+            fread(A, sizeof(int), 15, fp);
+            i++;
+        } else if ( i == 1){
+            fread(B, sizeof(int), 15, fp);
+            i++;
+            if (feof(fp)){
+            printf("Reached end of file!2\n");
+            break;
+        } else {
+            printf("In file are there more number than space in array.");
+            break;
+        }
+        }
+    }
+    for (i = 0; i < N; i++){
+            temp = A[i] + B[i];
+    printf("temp = %i\n", temp);
+        fwrite(&temp, sizeof(int), 1, fpOutput);
+    }
+    freopen("output.dat", "rb", fp);
+    fread(C, sizeof(int), N, fp);
+    for (i = 0; i < N; i++){
+       // fread(&j, sizeof(int), 1, fp);
+        printf("j = %i  A = %i  B = %i\n", C[i], A[i], B[i]);
+    }
+    fclose(fp);
+    fclose(fpOutput);
+    return 0;
+}
+
+
