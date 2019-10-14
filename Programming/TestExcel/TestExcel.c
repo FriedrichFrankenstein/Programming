3488e7af4ed1de8cade932c33cdd4a6eb39ace1e
Vlad Slyusar <fmfrankenschtein@gmail.com>
2019-10-14 14:45:17 +0300

    New thigns

---
+++ Programming/TestExcel/TestExcel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "libxlsxwriter\include\xlsxwriter.h"
+#include "libxl-3.8.5.0\include_c\libxl.h"
+
+int main()
+{
+
+   return 0;
+}
